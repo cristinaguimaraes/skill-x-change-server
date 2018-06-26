34459344f68e6affbaa638ecb19bdb062ac932a6
David Rebolleda Mir <david@MBP-de-David.lan>
2018-06-26 19:27:46 +0200

    fix: empty index.js of the app to avoidconflicts

--- server/index.js
+++ server/index.js
@@ -1,13 +1,1 @@
-const models = require('./models');
 
-models.sequelize.sync().then(function () {
-
-  console.log("Let's check the database!!!");
-  // /**
-  //  * Listen on provided port, on all network interfaces.
-  //  */
-  // app.listen(port, function () {
-  //   //eslint-disable-next-line
-  //   console.log('Koa app listening on port:', port);
-  // });
-});
